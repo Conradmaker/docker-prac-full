d1de7df82bfdadb6f4bc7747929372c58c73dc5e
conrad <yhg0337@naver.com>
2022-10-12 18:09:27 +0900

    Add production-ver image

--- server/db.js
+++ server/db.js
@@ -2,8 +2,9 @@ import mysql from "mysql2/promise";
 
 export const pool = mysql.createPool({
   connectionLimit: 10,
-  host: "mysql",
-  user: "root",
-  password: "root",
-  database: "myapp",
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT
 });
